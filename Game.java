83693d5bd4e73497cbd909bd2261376306e1362c
GrimesGolden <46364302+GrimesGolden@users.noreply.github.com>
2020-07-11 09:13:24 -0700

    Add files via upload
    FullScreen capability, basic start menu functionality, beginnings of game over functionality, and further AI and graphical fixes. 
    Phew.

--- Game.java
+++ Game.java
@@ -5,10 +5,12 @@ import java.awt.image.BufferedImage;
 public class Game extends Canvas implements Runnable {
 
     private static final long serialVersionUID = 1L;
+    protected static STATE State = STATE.MENU; // Protected static as all classes need this variable
 
     private boolean isRunning = false;
     private Thread thread;
     private Handler handler;
+    private Menu menu;
     private Camera camera;
     private SpriteSheet ss; // spritesheet
     private SpriteSheet cs; // character sheet
@@ -21,8 +23,12 @@ public class Game extends Canvas implements Runnable {
     // Ammo, hp, and frame must be public for rendering.
     public int ammo = 50;
     public int hp = 100;
-    //public int updates;
-    //public int frames;
+
+    // Modifying state debug
+    public enum STATE{
+        MENU,
+        GAME
+    };
 
     public Game() {
         new Window(1920,1080,"Wizard Game", this);
@@ -31,6 +37,7 @@ public class Game extends Canvas implements Runnable {
         // Don't create a new instance of this handler again, could cause problems.
         // Instead swap it into new classes, since the objects are inside this handler.
         handler = new Handler();
+        menu = new Menu();
         camera = new Camera(0, 0);
         this.addKeyListener(new KeyInput(handler));
 
@@ -70,46 +77,47 @@ public class Game extends Canvas implements Runnable {
         // Main gameloop thread.
 
 
-      this.requestFocus();
-      long lastTime = System.nanoTime();
-      double amountOfTicks = 60.0;
-      double ns = 1000000000 / amountOfTicks;
-      double delta = 0;
-      long timer = System.currentTimeMillis();
-      int updates = 0;
-      int frames = 0;
-      while(isRunning) {
-          long now = System.nanoTime();
-          delta += (now - lastTime) / ns;
-          lastTime = now;
-          while(delta >= 1) {
-              tick();
-              updates++;
-              delta--;
-          }
-          render();
-          frames++;
+        this.requestFocus();
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int updates = 0;
+        int frames = 0;
+        while(isRunning) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while(delta >= 1) {
+                tick();
+                updates++;
+                delta--;
+            }
+            render();
+            frames++;
 
-          if(System.currentTimeMillis() - timer > 1000) {
-              timer += 1000;
-              frames = 0;
-              updates = 0;
-          }
-      }
-      stop();
+            if(System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                frames = 0;
+                updates = 0;
+            }
+        }
+        stop();
     }
 
     public void tick() {
 
-        for(int i = 0; i < handler.object.size(); i++) {
-            if(handler.object.get(i).getId() == ID.Player) {
-                camera.tick(handler.object.get(i));
+        if(State == STATE.GAME) {
+            for(int i = 0; i < handler.object.size(); i++) {
+                if(handler.object.get(i).getId() == ID.Player) {
+                    camera.tick(handler.object.get(i));
+                }
             }
+
+            handler.tick();
         }
-
-        handler.tick();
-
-    }
+    } // end tick method
 
     public void render() {
         // Renders everything in the game.
@@ -122,78 +130,85 @@ public class Game extends Canvas implements Runnable {
 
         Graphics g = bs.getDrawGraphics();
         Graphics2D g2d = (Graphics2D) g;
-        ///////////this is where we actually draw stuff to game///////////////////////
 
-        // Create background.
-        g.setColor(Color.red);
-        g.fillRect(0, 0, 1920, 1080);
+        if(State == STATE.GAME) {
+            ///////////this is where we actually draw stuff to game///////////////////////
 
-        // Translate location of camera.
-        g2d.translate(-camera.getX(), -camera.getY());
 
-        for(int xx = 0; xx < 30*92; xx += 32) { //debug
-            for(int yy = 0; yy < 30*72; yy += 32) {
-                g.drawImage(floor, xx, yy, null);
+            // Create background.
+            g.setColor(Color.red);
+            g.fillRect(0, 0, 1920, 1080);
+
+            // Translate location of camera.
+            g2d.translate(-camera.getX(), -camera.getY());
+
+            for(int xx = 0; xx < 30*92; xx += 32) { //debug
+                for(int yy = 0; yy < 30*72; yy += 32) {
+                    g.drawImage(floor, xx, yy, null);
+                }
             }
-        }
 
-        // Render all GameObjects
-        handler.render(g);
+            // Render all GameObjects
+            handler.render(g);
 
-        // Keep translating camera as coords change. (Follow player)
-        g2d.translate(camera.getX(), camera.getY());
+            // Keep translating camera as coords change. (Follow player)
+            g2d.translate(camera.getX(), camera.getY());
 
-        // HUD: Creating health bar.
-        g.setColor(Color.gray);
-        g.fillRect(5, 5, 200, 32);
-        g.setColor(Color.red);
-        g.fillRect(5, 5, hp*2, 32);
-        g.setColor(Color.black);
-        g.drawRect(5, 5, 200, 32);
-        g.setColor(Color.white);
-        g.drawString("HEALTH", 5, 25);
-
-        // Creating ammo HUD.
-        g.setColor(Color.white);
-        g.drawString("Ammo: " + ammo, 5, 50);
-
-        // Handle death
-        if(hp <= 0) {
+            // HUD: Creating health bar.
+            g.setColor(Color.gray);
+            g.fillRect(5, 5, 200, 32);
+            g.setColor(Color.red);
+            g.fillRect(5, 5, hp*2, 32);
+            g.setColor(Color.black);
+            g.drawRect(5, 5, 200, 32);
             g.setColor(Color.white);
-            g.drawString("Game Over!", 400, 281);
-        }
-        //////////////////////////////////
+            g.drawString("HEALTH", 5, 25);
+
+            // Creating ammo HUD.
+            g.setColor(Color.white);
+            g.drawString("Ammo: " + ammo, 5, 50);
+
+            // Handle death
+            if(hp <= 0) {
+                g.setColor(Color.white);
+                g.drawString("Game Over!", 400, 281);
+            }
+            //////////////////////////////////
+            g.dispose();
+            bs.show();
+        } else if(State == STATE.MENU) {
+            menu.render(g);
+        }//end if state
         g.dispose();
         bs.show();
 
+    } // end render method
 
-        }
+    // Loading the level.
+    private void loadLevel(BufferedImage image) {
+        int w = image.getWidth();
+        int h = image.getHeight();
 
-        // Loading the level.
-        private void loadLevel(BufferedImage image) {
-            int w = image.getWidth();
-            int h = image.getHeight();
+        for(int xx = 0; xx < w; xx++) {
+            for(int yy = 0; yy < h; yy++) {
+                int pixel = image.getRGB(xx, yy);
+                int red = (pixel >> 16) & 0xff;
+                int green = (pixel >> 8) & 0xff;
+                int blue = (pixel) & 0xff;
 
-            for(int xx = 0; xx < w; xx++) {
-                for(int yy = 0; yy < h; yy++) {
-                    int pixel = image.getRGB(xx, yy);
-                    int red = (pixel >> 16) & 0xff;
-                    int green = (pixel >> 8) & 0xff;
-                    int blue = (pixel) & 0xff;
+                if(red == 255)
+                    handler.addObject(new Block(xx*32, yy*32, ID.Block, ss));
 
-                    if(red == 255)
-                        handler.addObject(new Block(xx*32, yy*32, ID.Block, ss));
+                if(blue == 255 && green == 0)
+                    handler.addObject(new Wizard(xx*32, yy*32, ID.Player, handler, this, cs));
 
-                    if(blue == 255 && green == 0)
-                        handler.addObject(new Wizard(xx*32, yy*32, ID.Player, handler, this, cs));
+                if(green == 255 && blue == 0)
+                    handler.addObject(new Enemy(xx*32, yy*32, ID.Enemy, handler, cs));
 
-                    if(green == 255 && blue == 0)
-                        handler.addObject(new Enemy(xx*32, yy*32, ID.Enemy, handler, cs));
-
-                    if(green == 255 && blue == 255)
-                        handler.addObject(new Crate(xx*32, yy*32, ID.Crate, ss));
-                }
+                if(green == 255 && blue == 255)
+                    handler.addObject(new Crate(xx*32, yy*32, ID.Crate, ss));
             }
+        }
     }
 
     public static void main(String[] args){
--- MouseInput.java
+++ MouseInput.java
@@ -1,7 +1,22 @@
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+/* Note about whats happening here as it's extremely convoluted - Jordan,
+so we notice that in the Menu buttons are drawn as seen below playButton etc, we now check to see if the points of the mouse click event
+denoted with variables mx and my are within the boundaries of that button, if that is true and we are in the menu state, then switch to game.
+The reason to check if we are in menu state first, is otherwise a mouse click will restart the game during the actual game, not good since we click to
+shoot ammo. This is not a good technique simply to create a menu button but it does work. The use of states is definately the way to go, but it will require refactoring.
+ */
+
+//n the mouse pressed event, put all the button checks within a if statement that checks if Game.State == Game.STATE.MENU
+
 public class MouseInput extends MouseAdapter {
+
+    // These buttons are here so we can check for their size values and calculate clicks.
+    public Rectangle playButton = new Rectangle(810, 150, 150, 75);
+    public Rectangle helpButton = new Rectangle(810, 450, 150, 75);
+    public Rectangle quitButton = new Rectangle(810, 750, 150, 75);
 
     private Handler handler;
     private Camera camera;
@@ -19,14 +34,20 @@ public class MouseInput extends MouseAdapter {
         int mx = (int) (e.getX() + camera.getX());
         int my = (int) (e.getY() + camera.getY());
 
-        for(int i = 0; i < handler.object.size(); i++) {
-            GameObject tempObject = handler.object.get(i);
+        if (Game.State == Game.STATE.GAME) {
+            for(int i = 0; i < handler.object.size(); i++) {
+                GameObject tempObject = handler.object.get(i);
 
-            if(tempObject.getId() == ID.Player && game.ammo >= 1) {
-
-                handler.addObject(new Bullet(tempObject.getX()+16, tempObject.getY()+24, ID.Bullet, handler, mx, my, ss));
-                game.ammo--;
+                if(tempObject.getId() == ID.Player && game.ammo >= 1) {
+                    handler.addObject(new Bullet(tempObject.getX()+16, tempObject.getY()+24, ID.Bullet, handler, mx, my, ss));
+                    game.ammo--;
+                }
             }
-        }
+        } else if (Game.State == Game.STATE.MENU) {
+            if(playButton.contains(mx, my)) {
+                Game.State = Game.STATE.GAME;
+            } // end if
+        } // end else if (god what a mess).
     }
 }
+
