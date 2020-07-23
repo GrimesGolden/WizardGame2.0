7875efdd3786bf72bd85ad8e0d92f4b31a61d127
GrimesGolden <46364302+GrimesGolden@users.noreply.github.com>
2020-07-22 19:38:08 -0700

    Add files via upload
    let the refactoring commence.

---
+++ WizardGame2.0/src/main/java/edu/sdccd/cisc191/wizardGame/Game.java
@@ -0,0 +1,300 @@
+package edu.sdccd.cisc191.wizardGame;
+
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
+import edu.sdccd.cisc191.wizardGame.events.KeyInput;
+import edu.sdccd.cisc191.wizardGame.events.MouseInput;
+import edu.sdccd.cisc191.wizardGame.gui.anim.Camera;
+import edu.sdccd.cisc191.wizardGame.gui.screen.*;
+import edu.sdccd.cisc191.wizardGame.gui.screen.Menu;
+import edu.sdccd.cisc191.wizardGame.gui.screen.Window;
+import edu.sdccd.cisc191.wizardGame.objects.Block;
+import edu.sdccd.cisc191.wizardGame.objects.Crate;
+import edu.sdccd.cisc191.wizardGame.objects.Ent;
+import edu.sdccd.cisc191.wizardGame.objects.Handler;
+import edu.sdccd.cisc191.wizardGame.objects.Hound;
+import edu.sdccd.cisc191.wizardGame.objects.ID;
+import edu.sdccd.cisc191.wizardGame.objects.Knight;
+import edu.sdccd.cisc191.wizardGame.objects.Minion;
+import edu.sdccd.cisc191.wizardGame.objects.Totem;
+import edu.sdccd.cisc191.wizardGame.objects.Wizard;
+import edu.sdccd.cisc191.wizardGame.utils.images.BufferedImageLoader;
+import edu.sdccd.cisc191.wizardGame.utils.images.SpriteSheet;
+
+import javax.swing.*;
+
+public class Game extends Canvas implements Runnable {
+
+    private static final long serialVersionUID = 1L;
+    protected static STATE State = STATE.MENU; // Protected static as all classes need this variable
+
+    private final int SCREEN_WIDTH = 1920;
+    private final int SCREEN_HEIGHT = 1080;
+
+    private boolean isRunning = false;
+    private Thread gameThread;
+
+    // Camera and handler.
+    private Camera camera;
+    private Handler handler;
+
+    // Objects used for menus
+    private Menu menu;
+    private Help help;
+    private Pause pause;
+
+    // Spritesheets
+    private SpriteSheet ss;
+    private SpriteSheet cs; // character sheet
+
+    private BufferedImage sprite_sheet = null;
+    private BufferedImage char_sheet = null;
+    private BufferedImage floor = null;
+    private BufferedImage lives_image;
+
+    // JFrame variables
+    private JFrame frame;
+    private GraphicsDevice device = GraphicsEnvironment // Used for fullscreen.
+            .getLocalGraphicsEnvironment().getScreenDevices()[0];
+
+    // Variables that are critical for display in the HUD, ammo, hp etc.
+    private int ammo = 50;
+    private int hp = 0; // Wizard fills the hp upon construction.
+    private int lives; // Lives fed as argument in constructor.
+    private LevelOne level;
+    private boolean totem_flag = false;
+
+    // Modifying state debug
+    public enum STATE{
+        MAIN_MENU,
+        MENU,
+        GAME,
+        HELP,
+        PAUSE,
+    };
+
+    public Game() {
+
+        //Window.changeLevel(this); // Level one
+        frame = new JFrame("Wizard Game");
+        frame.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_WIDTH));
+        frame.setMaximumSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        frame.setMinimumSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+
+        frame.setResizable((false));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+
+        //device.setFullScreenWindow(frame); // Careful with this
+
+        // Don't create a new instance of this handler again, could cause problems.
+        // Instead place original instance into new classes, since the game objects are inside this handler.
+        handler = new Handler(); // independant for each level not the controller
+        level = new LevelOne(this);
+        menu = new Menu();// yes every state object should be created here
+        help = new Help();
+        pause = new Pause();
+        //camera = new Camera(0, 0); // and the camera
+        this.addKeyListener(new KeyInput(handler)); // and the listener
+        lives = 3; // lives and HP are independant of the level.
+
+        BufferedImageLoader loader = new BufferedImageLoader();
+
+        sprite_sheet = loader.loadImage("/main_sheet.png"); // load in the levels not here
+        char_sheet = loader.loadImage("/wizard_sheet.png"); //char sheet
+
+        ss = new SpriteSheet(sprite_sheet);
+
+        cs = new SpriteSheet(char_sheet); // character sheet
+
+        lives_image = cs.grabImage(13, 8, 32, 32); // Sprite to display lives.
+
+        /* current_level parameter determines which level is loaded.
+        switch (current_level) {
+            case 1:
+                level = loader.loadImage("/level_one.png"); // load level
+                floor = ss.grabImage(6, 6, 32, 32); // load floor tiles
+                level_numb = current_level;
+                break;
+            case 2:
+                level = loader.loadImage("/level_two.png"); // load level 2
+                floor = ss.grabImage(7, 2, 32, 32); // load different floor tiles
+                level_numb = current_level;
+                break;
+            case 3:
+                level = loader.loadImage("/level_two.png"); // load level 2
+                floor = ss.grabImage(7, 2, 32, 32); // load different floor tiles
+                level_numb = current_level;
+                break;
+            case 4:
+                // If you go past level 4 will switch to white screen of death.
+                level = loader.loadImage("/level_two.png"); // load level 2
+                floor = ss.grabImage(7, 2, 32, 32); // load different floor tiles
+                level_numb = current_level;
+                break;
+        }*/
+
+        this.addMouseListener(new MouseInput(handler, camera, this, ss, cs));
+
+        //loadLevel(level);
+        frame.add(this); // Add this game to the frame.
+        frame.validate();
+        start(); // Everything is loaded in, now begin rendering.
+    }
+
+    public synchronized void start() {
+        if (!isRunning) {
+            isRunning = true;
+            gameThread = new Thread(this, "GameController");
+            gameThread.start();
+        }
+    }
+
+    public synchronized void stop() {
+        if (isRunning) {
+            isRunning = false;
+            System.exit(0);
+        }
+    }
+
+    public void run() {
+        // Main game loop thread.
+
+        this.requestFocus();
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int updates = 0;
+        int frames = 0;
+        while(isRunning) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while(delta >= 1) {
+                tick();
+                updates++;
+                delta--;
+            }
+            render();
+            frames++;
+
+            if(System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                frames = 0;
+                updates = 0;
+            }
+        }
+        stop();
+    }
+
+    public void tick() {
+        // Change this to a switch statement, every different case has it's own tick.
+        // Moves objects to next position 60 times a second.
+        /*if(State == STATE.GAME) {
+            for(int i = 0; i < handler.getObject().size(); i++) {
+                if(handler.getObject().get(i).getId() == ID.Player) {
+                    camera.tick(handler.getObject().get(i));
+                }
+            }
+            handler.tick();
+        }*/
+    } // end tick method
+
+    public void render() {
+        // Render the particular levels render method or you're going to have a bad time...
+
+        // Renders everything in the game.
+        // Again this should just be switch statements deciding what to render.
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null) {
+            // Three in the chamber preloads 3 frames for each render.
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        /* Get the graphics2D object from the buffer strategy. */
+        Graphics2D g = (Graphics2D) bs.getDrawGraphics();
+
+        menu.render(g);
+
+        if(State == STATE.GAME) {
+            // level.render
+            //////////////////////////////////
+        } else if(State == STATE.MENU) {
+            menu.render(g);
+        } else if (State == STATE.HELP) {
+            help.render(g);
+        } else if(State == STATE.PAUSE) {
+            pause.render(g);
+        }//end if state
+        g.dispose();
+        bs.show();
+    } // end render method
+
+    // Loading the level.
+    //private void loadLevel(BufferedImage image) {
+       // This should take place outside of the Game class.
+   // }
+
+    public static void quitGame(){
+        // If quit game is activated, the window will close and the program will exit.
+        System.exit(0);
+    }
+
+    /* Setters and getters for private variables. */
+    public STATE getGameState() {
+        return State;
+    }
+
+    public void setGameState(STATE State){
+        this.State = State;
+    }
+    public int getAmmo() {
+        return ammo;
+    }
+
+    public void setAmmo (int ammo)
+    {
+        this.ammo = ammo;
+    }
+
+    public void incAmmo(int inc){
+        ammo += inc;
+    }
+
+    public int getHp(){
+        return hp;
+    }
+
+    public void setHp(int hp){
+        this.hp = hp;
+    }
+
+    public void decHp(){
+        hp--;
+    }
+
+    public int getLives()
+    {
+        return lives;
+    }
+
+    public void decLives(){
+        lives--;
+    }
+
+    public void setLives(int lives)
+    {
+        this.lives = lives;
+    }
+
+    public static void main(String[] args) {
+        new Game(); // Represents the first level thread.
+    }
+}
---
+++ WizardGame2.0/src/main/java/edu/sdccd/cisc191/wizardGame/objects/Block.java
@@ -0,0 +1,43 @@
+package edu.sdccd.cisc191.wizardGame.objects;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+import edu.sdccd.cisc191.wizardGame.Game;
+import edu.sdccd.cisc191.wizardGame.gui.screen.LevelOne;
+import edu.sdccd.cisc191.wizardGame.utils.images.SpriteSheet;
+
+public class Block extends GameObject {
+
+    LevelOne level; // Needs access to game in order to access level
+
+    private BufferedImage block_image;
+
+    public Block(int x, int y, ID id, SpriteSheet ss, LevelOne level) {
+        super(x, y, id, ss);
+
+        this.level = level; // Needs access to level to determine which level we are on.
+
+        // Changes block look for each new level.
+        /*switch (game.level_numb) {
+            case 1: block_image = ss.grabImage(6, 9 ,32, 32);
+                    break;
+            case 2: block_image = ss.grabImage(26, 11, 32, 32);
+                    break;*/
+
+        block_image = ss.grabImage(6, 9 ,32, 32);
+        }
+
+    public void tick() {
+
+    }
+
+    public void render(Graphics g) {
+        g.drawImage(block_image, x, y, null);
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, 32, 32);
+    }
+}
